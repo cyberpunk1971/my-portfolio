d3f94dc678ae6ca4ebd2c7472ac29400ad2c688b
cyberpunk1971 <adt71@cox.net>
2018-12-18 11:17:08 -0700

    added name to About page

--- src/components/About.js
+++ src/components/About.js
@@ -5,7 +5,7 @@ export default function About() {
   return (
     <div className="about">
       <main>
-        <p>Hello, and thanks for visiting. The home page has samples of work i've done. I am focused on full stack development using the MERN stack, currently. If you are interested in working together, or even just a chat you can reach me <a href="mailto:adt71@cox.net" className="about-link">HERE</a> .</p>
+        <p>Hello, my name is Alex and thanks for visiting. The home page has samples of work i've done. I am focused on full stack development using the MERN stack, currently. If you are interested in working together, or even just a chat you can reach me <a href="mailto:adt71@cox.net" className="about-link">HERE</a> .</p>
       </main>
     </div>
   )
